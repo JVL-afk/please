820fb83fbef5efdcb8707c69285d7ce356f68eb9
JVL-afk <andreimiroiu2019@gmail.com>
2025-04-28 21:27:56 +0300

    Add files via upload

---
+++ AFFILIFY/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  // Add any Next.js specific configurations here
+  // For example, to enable standalone output for Docker:
+  // output: "standalone",
+};
+
+module.exports = nextConfig;
+
